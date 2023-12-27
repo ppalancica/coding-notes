749e3f8b3beaa9cc515b1f6d84166a314ed8ef76
Pavel Palancica <pavel.palancica@gmail.com>
2023-12-27 23:44:32 +0200

    Added SwiftUI-Demo project

---
+++ demos/SwiftUI-Demo/SwiftUI-Demo/SwiftUI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DemoApp.swift
+//  SwiftUI-Demo
+//
+//  Created by Pavel Palancica on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
