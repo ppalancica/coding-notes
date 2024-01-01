c3d9b1e42dcc45316096eaa2494ec91747826bab
Pavel Palancica <pavel.palancica@gmail.com>
2024-01-01 23:34:27 +0200

    Added NotesApp-iOS-UIKit-MVC project

---
+++ ios/iOS-Architectures/NotesApp-iOS-UIKit-MVC/NotesApp-iOS-UIKit-MVC/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  NotesApp-iOS-UIKit-MVC
+//
+//  Created by Pavel Palancica on 01.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
